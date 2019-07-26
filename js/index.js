8495edc979223a896b45f1b82f0fb9ad39a4fc0c
Roberto Camacho <camachof.roberto@gmail.com>
2019-07-26 17:58:14 -0300

    -Estilizando CSS

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ let numberFlashLight;
 let simonTurn;
 let intervalFlashLight;
 
+let strict = false
+let on = true;
 let win;
 
 // Seleção das cores do jogo
@@ -15,100 +17,149 @@ const downLeft = document.querySelector('#down-left');
 const downRigth = document.querySelector('#down-rigth');
 const turnCounter = document.querySelector('#count');
 
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+
+strictButton.addEventListener('click', (event) => {
+    if (strictButton.checked ==true){
+        strict = true;
+    }else{
+        strict = false;
+    };
+});
+
+onButton.addEventListener('click', (event) =>{
+    if(onButton.checked == true){
+        on = true;
+        turnCounter.innerHTML = "-";
+    } else{
+        on = false;
+        turnCounter.innerHTML = "";
+        clearColor();
+        clearInterval(intervalFlashLight);
+    }
+})
+
+startButton.addEventListener('click', (event) =>{
+    if(on || win){
+        simonPlay();
+    }
+});
+
 // Sequência aleatória das cores do jogo
 function simonPlay() {
-  simonTurn = 1;
-  for (let i = 0; i < numOfLevels; i++) {
-    simonSequence.push(Math.floor(Math.random() * 4) + 1);
-  }
+    simonTurn = 1;
+    for (let i = 0; i < numOfLevels; i++) {
+        simonSequence.push(Math.floor(Math.random() * 4) + 1);
+    }
 
-  intervalFlashLight = setInterval(gameTurn, 800);
+    intervalFlashLight = setInterval(gameTurn, 800);
 }
 
 // Turno do jogo
 function gameTurn() {
 
-  if (numberFlashLight == simonTurn) {
-    clearInterval(intervalFlashLight);
-    clearColor();
-  }
+    if (numberFlashLight == simonTurn) {
+        clearInterval(intervalFlashLight);
+        clearColor();
+    }
 
-  if (simonSequence) {
-    setTimeout(() => {
-      if (simonSequence[numberFlashLight] === 1) one();
-      if (simonSequence[numberFlashLight] === 2) two();
-      if (simonSequence[numberFlashLight] === 3) three();
-      if (simonSequence[numberFlashLight] === 4) four();
-      numberFlashLight++;
-    }, 200);
-  }
+    if (simonSequence) {
+        clearColor();
+        setTimeout(() => {
+            if (simonSequence[numberFlashLight] === 1) one();
+            if (simonSequence[numberFlashLight] === 2) two();
+            if (simonSequence[numberFlashLight] === 3) three();
+            if (simonSequence[numberFlashLight] === 4) four();
+            numberFlashLight++;
+        }, 200);
+    }
 }
 
 // Alternância das cores selecionadas
 function one() {
     topLeft.style.backgroundColor = 'lightgreen';
-    if(!win){
-        setTimeout(() => {
-            clearColor();
-        }, 300);
-    }
-  }
-  
-  function two() {
+}
+
+function two() {
     topRigth.style.backgroundColor = 'tomato';
-    if(!win){
-        setTimeout(() => {
-            clearColor();
-        }, 300);
-    }
-  }
-  
-  function three() {
+}
+
+function three() {
     downLeft.style.backgroundColor = 'yellow';
-    if(!win){
-        setTimeout(() => {
-            clearColor();
-        }, 300);
-    }
-  }
-  
-  function four() {
+}
+
+function four() {
     downRigth.style.backgroundColor = 'lightskyblue';
-    if(!win){
-        setTimeout(() => {
-            clearColor();
-        }, 300);
-    }
-  }
+}
 
 function clearColor() {
-  topLeft.style.backgroundColor = 'darkgreen';
-  topRigth.style.backgroundColor = 'darkred';
-  downLeft.style.backgroundColor = 'goldenrod';
-  downRigth.style.backgroundColor = 'darkblue';
+    topLeft.style.backgroundColor = 'darkgreen';
+    topRigth.style.backgroundColor = 'darkred';
+    downLeft.style.backgroundColor = 'goldenrod';
+    downRigth.style.backgroundColor = 'darkblue';
+}
+
+function flashColor() {
+    topLeft.style.backgroundColor = 'lightgreen';
+    topRigth.style.backgroundColor = 'tomato';
+    downLeft.style.backgroundColor = 'yellow';
+    downRigth.style.backgroundColor = 'lightskyblue';
 }
 
 // Preferência das cores do jogador
 topLeft.addEventListener('click', (event) => {
-  playerSequence.push(1);
-  one();
+    playerSequence.push(1);
+    verifySelected();
+    one();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }
 });
 
 topRigth.addEventListener('click', (event) => {
-  playerSequence.push(2);
-  two();
+    playerSequence.push(2);
+    verifySelected();
+    two();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }
 });
 
 downLeft.addEventListener('click', (event) => {
-  playerSequence.push(3);
-  three();
+    playerSequence.push(3);
+    verifySelected();
+    three();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }
 });
 
 downRigth.addEventListener('click', (event) => {
-  playerSequence.push(4);
-  four();
+    playerSequence.push(4);
+    four();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }
 });
 
-simonPlay();
-console.log(simonSequence);
-console.log(playerSequence);
+function verifySelected () {
+    if(simonTurn == playerSequence.length){
+        simonTurn++;
+        playerSequence = []
+        intervalFlashLight = setInterval( gameTurn,800)
+    }
+}
+
+//simonPlay();
+
